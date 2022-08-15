4ff8222daf55ee8f673c7dcf3ebe75fba01e1024
Austin <adpierce74@yahoo.com>
2022-08-15 01:04:06 -0400

    Added UI and updated logic to align with UI changes

--- scripts.js
+++ scripts.js
@@ -1,87 +1,130 @@
 // Script file
 
+//selected rock
+const rock = document.querySelector(".rock-btn");
+rock.addEventListener('click', () => game("rock"));
+
+//selected paper
+const paper = document.querySelector(".paper-btn");
+paper.addEventListener('click', () => game("paper"));
+
+//selected scissors
+const scissor = document.querySelector(".scissor-btn");
+scissor.addEventListener('click', () => game("scissors"));
+
+const resultsPanel = document.querySelector(".results");
+const pcDisplay = document.querySelector(".player-choice");
+const ccDisplay = document.querySelector(".computer-choice");
+const playerScoreCard = document.querySelector(".player-score");
+const computerScoreCard = document.querySelector(".computer-score");
+const resetButton = document.querySelector(".reset-button");
+
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     randomInt = getRandomInt(1, 4);
     if (randomInt === 1) {
         return "rock";
-    }else if (randomInt === 2) {
+    } else if (randomInt === 2) {
         return "paper";
-    }else {
+    } else {
         return "scissors";
     }
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);   
+    min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 function playRound(player, computer) {
 
-    console.log("Computer choice was: "+ computer)
-    console.log("Player choice was: " +player)
+    console.log("Computer choice was: " + computer)
+    ccDisplay.textContent = "Computer choice was " + computer;
+
+    console.log("Player choice was: " + player)
+    pcDisplay.textContent = "Player choice was " + player;
 
     //Logic path if player is rock.
     if (player === "rock") {
-        if (computer === "rock"){
-            return "Tie"
+        if (computer === "rock") {
+            return "tied"
         } else if (computer === "paper") {
-            return "Lose"
+            computerScore += 1;
+            return "lost"
         } else {
-            return "Win"
+            playerScore += 1;
+            return "won"
         }
     }
 
     //Logic path if player is paper.
     if (player === "paper") {
-        if (computer === "paper"){
-            return "Tie"
+        if (computer === "paper") {
+            return "tied"
         } else if (computer === "scissors") {
-            return "Lose"
+            computerScore += 1;
+            return "lost"
         } else {
-            return "Win"
+            playerScore += 1;
+            return "won"
         }
     }
 
     //Logic path if player is scissors.
     if (player === "scissors") {
-        if (computer === "scissors"){
-            return "Tie"
+        if (computer === "scissors") {
+            return "tied"
         } else if (computer === "rock") {
-            return "Lose"
-        } else {
-            return "Win"
-        }
-    }
-}
-
-function game(amount) {
-
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < amount; i++) {
-        let input = prompt();
-        let roundResults = playRound(input.toLowerCase(), getComputerChoice());
-        console.log(roundResults);
-        if ( roundResults === "Win"){
-            playerScore += 1;
-        } else if ( roundResults === "Lose") {
             computerScore += 1;
+            return "lost"
+        } else {
+            playerScore += 1;
+            return "won"
         }
     }
 
-    if (playerScore < computerScore) {
-        return "You lose! Your score was: " + playerScore + " Computer score was " + computerScore;
-    } else if (playerScore > computerScore) {
-        return "You win! Your score was: " + playerScore + " Computer score was " + computerScore;
-    } else {
-        return "Tie game! Your score was: " + playerScore + " Computer score was " + computerScore;
+    return 0;
+}
+
+function toggleButtons() {
+    rock.classList.toggle("disable-button");
+    paper.classList.toggle("disable-button");
+    scissor.classList.toggle("disable-button");
+}
+
+function resetGame(){
+    toggleButtons();
+    playerScore = 0;
+    computerScore = 0;
+    resultsPanel.textContent = "Results Here";
+    pcDisplay.textContent = "Your Choice";
+    ccDisplay.textContent = "Computer's Choice";
+    playerScoreCard.textContent = "Player Score: 0";
+    computerScoreCard.textContent = "Computer Score: 0"
+    resetButton.style.visibility = "hidden";
+}
+
+function game(input) {
+
+    let roundResults = playRound(input, getComputerChoice());
+    resultsPanel.textContent = "You " + roundResults + " this round.";
+    computerScoreCard.textContent = "Computer Score: " + computerScore;
+    playerScoreCard.textContent = "Player Score: " + playerScore;
+    console.log(roundResults);
+
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore > computerScore) {
+            resultsPanel.textContent = "You won the game!"
+            toggleButtons();
+            resetButton.style.visibility = "visible";
+        } else if (playerScore < computerScore) {
+            resultsPanel.textContent = "You lost the game!"
+            toggleButtons();
+            resetButton.style.visibility = "visible";
+        }
     }
 }
 
-
-
-//Play the game
-console.log(game(5))
